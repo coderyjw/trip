ea8079bef8163a7199e43fd777c288620da1d582
叶继伟 <729721503@qq.com>
2022-08-27 19:18:45 +0800

    完成热门城市列表展示

--- src/views/city/cpns/city-group.vue
+++ src/views/city/cpns/city-group.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="city-group">
-    <van-index-bar highlight-color="#ff9854">
+    <van-index-bar highlight-color="#ff9854" :index-list="indexList">
+      <van-index-anchor index="热门" />
+      <div class="list">
+        <template v-for="(city, index) in groupData.hotCities">
+          <div class="city">{{ city.cityName }}</div>
+        </template>
+      </div>
       <template v-for="(group, index) in groupData.cities">
         <van-index-anchor :index="group?.group" />
         <template v-for="(city, indey) in group.cities" :key="indey">
@@ -12,15 +18,40 @@
 </template>
 
 <script setup>
-defineProps({
+import { computed } from "vue";
+const props = defineProps({
   groupData: {
     type: Object,
     default: () => {},
   },
 });
+
+const indexList = computed(() => {
+  const result = props?.groupData?.cities?.map((item) => item.group) || [];
+  result?.unshift("#");
+  return result;
+});
 </script>
 
 <style lang="less" scoped>
 .city-group {
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    padding: 10px;
+    .city {
+      width: 70px;
+      height: 28px;
+      border-radius: 14px;
+      background-color: #fff4ec;
+      font-size: 12px;
+      display: flex;
+      line-height: 28px;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 10px;
+    }
+  }
 }
 </style>
