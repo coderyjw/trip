857d8f12cd3414d87b5d07ea83df27471d126038
叶继伟 <729721503@qq.com>
2022-08-28 02:14:04 +0800

    修复tabbar currentIndex逻辑

--- src/components/tab-bar/index.vue
+++ src/components/tab-bar/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="tab-bar">
-    <van-tabbar v-model="active" active-color="#ff9854">
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
       <template v-for="(item, index) in tabbarData" :key="index">
-        <van-tabbar-item :name="item.text" :icon="item.icon" :to="item.path">{{
+        <van-tabbar-item :icon="item.icon" :to="item.path">{{
           item.text
         }}</van-tabbar-item>
       </template>
@@ -11,10 +11,19 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 import tabbarData from "@/assets/data/tabbar.js";
 
-const active = ref(tabbarData[0].text);
+// 监听路由改变时, 找到对应的索引, 设置currentIndex
+const route = useRoute();
+const currentIndex = ref(0);
+watch(route, (newRoute) => {
+  const index = tabbarData.findIndex((item) => item.path === newRoute.path);
+  console.log(index, newRoute);
+  if (index === -1) return;
+  currentIndex.value = index;
+});
 </script>
 
 <style lang="less" scoped>
