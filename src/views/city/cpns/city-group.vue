a3cd687014f67380d710a09646eb8c9469d9ad80
叶继伟 <729721503@qq.com>
2022-08-27 19:00:01 +0800

    完成城市列表的展示

---
+++ src/views/city/cpns/city-group.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="city-group">
+    <van-index-bar highlight-color="#ff9854">
+      <template v-for="(group, index) in groupData.cities">
+        <van-index-anchor :index="group?.group" />
+        <template v-for="(city, indey) in group.cities" :key="indey">
+          <van-cell :title="city.cityName" />
+        </template>
+      </template>
+    </van-index-bar>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  groupData: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.city-group {
+}
+</style>
--- src/views/city/index.vue
+++ src/views/city/index.vue
@@ -8,25 +8,29 @@
       @cancel="onCancel"
       shape="round"
     />
-    <van-tabs v-model:active="tabActive" color="#ff9854">
-      <van-tab
-        v-for="(value, key, index) in allCities"
-        :key="key"
-        :title="value.title"
-        >{{ value.title }}</van-tab
-      >
+    <van-tabs v-model:active="tabActive" color="#ff9854" class="top">
+      <template v-for="(value, key, index) in allCities" :key="key">
+        <van-tab :title="value.title" :name="key"> </van-tab>
+      </template>
     </van-tabs>
+    <div class="content">
+      <template v-for="(value, key, index) in allCities">
+        <city-group v-show="key === tabActive" :group-data="value" />
+      </template>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
-import { ref } from "vue";
-import useCityStore from "@/stores/modules/city";
 import { storeToRefs } from "pinia";
+import { ref, computed } from "vue";
+import useCityStore from "@/stores/modules/city";
 
-const searchValue = ref("");
+import cityGroup from "./cpns/city-group.vue";
+
 const router = useRouter();
+const searchValue = ref("");
 const tabActive = ref(0);
 
 const onSearch = (val) => {};
@@ -37,9 +41,19 @@ const onCancel = () => {
 const cityStore = useCityStore();
 cityStore.fetchAllCitiesData();
 const { allCities } = storeToRefs(cityStore);
+
+const currentGroup = computed(() => allCities.value[tabActive.value]);
 </script>
 
 <style lang="less" scoped>
 .city {
+  .content {
+    height: calc(100vh - 98px);
+    overflow-y: auto;
+  }
+  .top {
+    position: relative;
+    z-index: 9;
+  }
 }
 </style>
