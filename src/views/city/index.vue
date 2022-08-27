4bf8332093fc115ddab25d934397d70c81af8c78
叶继伟 <729721503@qq.com>
2022-08-27 17:32:06 +0800

    city页面搜索框搭建

--- src/views/city/index.vue
+++ src/views/city/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="city top-page">
-    <h2>city</h2>
+    <van-search
+      v-model="searchValue"
+      show-action
+      placeholder="城市/区域/位置"
+      @search="onSearch"
+      @cancel="onCancel"
+      shape="round"
+    />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const searchValue = ref("");
+const router = useRouter();
+const onSearch = (val) => Toast(val);
+const onCancel = () => {
+  router.back();
+};
+</script>
 
 <style lang="less" scoped>
 .city {
