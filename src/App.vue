3cd8558957c60f177bcfef018551e87e3b823cfc
叶继伟 <729721503@qq.com>
2022-08-28 02:46:51 +0800

    添加loading组件

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div id="app">
     <router-view />
     <tab-bar />
+    <loading/>
   </div>
 </template>
 
 <script setup>
 import TabBar from "@/components/tab-bar/index.vue";
+import Loading from "@/components/loading/loading.vue"
 </script>
 
 <style scoped></style>
--- src/service/request/index.js
+++ src/service/request/index.js
@@ -1,9 +1,33 @@
 import axios from "axios";
+import useMainStore from "@/stores/modules/main";
 
 import { TIMEOUT, baseURL } from "./config";
+const mainStore = useMainStore();
+let loadingNum = 0;
 class Request {
   constructor(baseURL, timeout = 10000) {
     this.instance = axios.create({ baseURL, timeout });
+
+    this.instance.interceptors.request.use(
+      (config) => {
+        startLoading();
+        return config;
+      },
+      (err) => {
+        endLoading();
+        return err;
+      }
+    );
+    this.instance.interceptors.response.use(
+      (res) => {
+        endLoading();
+        return res;
+      },
+      (err) => {
+        endLoading();
+        return err;
+      }
+    );
   }
 
   request(config) {
@@ -28,4 +52,15 @@ class Request {
   }
 }
 
+function startLoading() {
+  mainStore.isLoading = true;
+  loadingNum++;
+}
+
+function endLoading() {
+  loadingNum--;
+  if (loadingNum === 0) {
+    mainStore.isLoading = false;
+  }
+}
 export default new Request(baseURL, TIMEOUT);
