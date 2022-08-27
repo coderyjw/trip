0d6c21266f0ec45e88df00a0750f3903b968e716
叶继伟 <729721503@qq.com>
2022-08-28 02:02:51 +0800

    上拉加载

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="home">
+  <div class="home" ref="homeRef">
     <home-nav-bar />
     <div class="banner">
       <van-image height="100%" :src="getAssetURL('home/banner.png')" />
     </div>
     <home-search-box />
     <home-categories />
+    <div class="search-bar" v-if="isShowSearchBar">
+      <search-bar :start-date="'09.19'" :end-date="'09.20'" />
+    </div>
     <home-content />
   </div>
 </template>
@@ -17,17 +20,58 @@ import HomeCategories from "./cpns/home-categories.vue";
 import HomeContent from "./cpns/home-content.vue";
 import { getAssetURL } from "@/utils/load_assets.js";
 import { useHomeStore } from "@/stores/modules/home";
+import { ref, watch, onActivated, computed } from "vue";
+import useScroll from "@/hooks/useScroll";
 
 const homeStore = useHomeStore();
 // 发送网络请求
 homeStore.fetchHotSuggestData();
 homeStore.fetchCategoriesData();
 homeStore.fetchHouselistData();
+
+// 监听滚动到底部
+const homeRef = ref();
+const { isReachBottom, scrollTop } = useScroll(homeRef);
+watch(isReachBottom, (newValue) => {
+  if (newValue) {
+    homeStore.fetchHouselistData().then(() => {
+      isReachBottom.value = false;
+    });
+  }
+});
+
+// 定义的可响应式数据, 依赖另外一个可响应式的数据, 那么可以使用计算函数(computed)
+const isShowSearchBar = computed(() => {
+  return scrollTop.value >= 360;
+});
+
+// 跳转回home时, 保留原来的位置
+onActivated(() => {
+  homeRef.value?.scrollTo({
+    top: scrollTop.value,
+  });
+});
 </script>
 
 <style lang="less" scoped>
 .home {
   --van-font-weight-bold: 700;
   --van-nav-bar-title-text-color: var(--primary-color);
+
+  height: 100vh;
+  overflow-y: auto;
+  box-sizing: border-box;
+  padding-bottom: 60px;
+}
+
+.search-bar {
+  position: fixed;
+  z-index: 9;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 45px;
+  padding: 16px 16px 10px;
+  background-color: #fff;
 }
 </style>
